7a960d909b898c96b16fedb4f957fc2408116de2
Rupaj Sen <rupajps4@gmail.com>
2022-11-12 17:22:22 +0530

    🚀

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -10,13 +10,13 @@ export default function Footer() {
     <Fade bottom duration={1000} distance="5px">
       <div className="footer-div">
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {emoji("Made with ❤️ by DeveloperFolio Team")}
+          {emoji("Made with ❤️ by Rupaj Sen")}
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          Theme by{" "}
+          {/* Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
-          </a>
+          </a> */}
         </p>
       </div>
     </Fade>
